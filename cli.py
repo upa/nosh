045ccf88ef075898155bcc666c60fe3bcedecf3b
Ryo Nakamura <upa@haeena.net>
2024-03-02 17:04:52 +0900

    cli: add mode transition

--- cli.py
+++ cli.py
@@ -12,10 +12,16 @@ from nosh.nosh import Node
 def prompt_cb() -> str:
     return "{}@{}>".format(os.getlogin(), socket.gethostname())
 
+def prompt_cb_configure() -> str:
+    return "{}@{}#".format(os.getlogin(), socket.gethostname())
+
+
+conf = nosh.Nosh(prompt_cb=prompt_cb_configure)
+cli = nosh.Nosh(prompt_cb=prompt_cb)
+
 
 def act_cli_exit(args: list[str]):
     raise EOFError
-
 
 def act_show_interfaces(args):
     out = subprocess.check_output(["ifconfig"], text=True)
@@ -32,10 +38,19 @@ def act_show_system(args):
     out = subprocess.check_output(["uname", "-a"], text=True)
     print(out)
 
+def act_configure_mode(args):
+    conf.start_cli()
 
 def main():
 
-    cli = nosh.Nosh(prompt_cb=prompt_cb)
+    node_set = Node("set", "Set a parameter")
+    node_set.append_leaf(Node("pppoe", "PPPoE"))
+    node_set.append_leaf(Node("wlan", "Wireless interface"))
+    
+    conf.root.append_leaf(node_set)
+    conf.root.append_leaf(Node("exit", "Exit from Configure mode",
+                               action = act_cli_exit))
+
     show = Node("show", "Show system information")
     show_interfaces = Node(
         "interfaces", "Show interface information", action=act_show_interfaces
@@ -49,8 +64,10 @@ def main():
     show.append_leaf(Node("ip", "Show ip information"))
     show.append_leaf(Node("system", "Show system information", action=act_show_system))
 
-    cli.root.append_leaf(Node("set", "Set configuration"))
     cli.root.append_leaf(Node("exit", "Exit from CLI", action=act_cli_exit))
+
+    cli.root.append_leaf(Node("configure", "Enter configuration mode",
+                              action = act_configure_mode))
 
     cli.start_cli()
 
